691779a1de2266e45c0c6bc1a1df8517816f6ab0
ives560 <kingwan560@163.com>
2017-05-10 13:57:29 +0800

    Initial commit

---
+++ STM32/F10x/main.h
@@ -0,0 +1,16 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
